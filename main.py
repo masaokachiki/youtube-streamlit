56877d7645075a71ba1aeb02c143bc2d46ba0cf3
masaokachiki <oono.pc@gmail.com>
2022-06-26 12:19:01 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import time
+
+st.title('streamlit 超入門')
+
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration{i+1}')
+    bar.progress(i+1)
+    time.sleep(0.01)
+
+'Done!!'
+
+left_colmn, right_colmn = st.columns(2)
+button = left_colmn.button('右カラムにボタンを表示')
+if button:
+    right_colmn.write('ここは右カラム')
+
+expander1 = st.expander('問い合わせ1')
+expander1.write('問い合わせ1の回答')
+expander2 = st.expander('問い合わせ2')
+expander2.write('問い合わせ2の回答')
+
